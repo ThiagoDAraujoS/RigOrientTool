2eb5b05e92e44711051eb1723ce8a71f038d16f4
Thiago Silva <thiagodaraujos@gmail.com>
2023-07-16 15:02:43 -0700

    Added recursion framework

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
+from __future__ import annotations
 from maya import cmds
 from typing import Callable
-
-
-class RecursiveFramework:
-    def __int__(self):
-        self.root_bone: str = ""
-        self.predicates: tuple[Pattern, Callable]
-
-    # TODO: make sure the base joint has its values 0ed and the original values are added to the joint orient
-    # TODO: all joint orient values are 0ed and they were moved to the rotate
-
-    # TODO: Create a recursion that travels through a bone structure
-
-
-    def transverse(self, bone):
-        """
-        this method goes through a tree
-        :param bone:
-        :return:
-        """
-
-        children = cmds.listRelatives(bone)
-        if children:
-            for child in children:
-                self.transverse(child)
 
 
 class Pattern:
     pass
 
 
-# Press the green button in the gutter to run the script.
+class RecursiveFramework:
+    def __int__(self):
+        self.root: str = ""
+        self.predicates: tuple[Pattern, Callable]
+
+    # TODO: make sure the base joint has its values 0ed and the original values are added to the joint orient
+    # TODO: all joint orient values are 0ed and they were moved to the rotate
+    # TODO: make iteration to return how long from the end
+    # TODO: make iteration to return depth level
+    # TODO: make iteration to return branch id
+    # TODO? make iteration to perform predication and expression
+    @staticmethod
+    def get_children(joint_name):
+        return cmds.listRelatives(joint_name, children=True, type="joint") or []
+
+    def transverse(self, bone):
+        if not bone:
+            return None
+
+        children = RecursiveFramework.get_children(bone)
+        if children:
+            for child in children:
+                self.transverse(child)
+                print(child)
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    rf = RecursiveFramework()
+    rf.root = "test"
+    rf.transverse(rf.root)
